8ccfa1118738d2628693414563ac243af6165f35
Laurent Kempé <laurent.kempe@innoveo.com>
2024-12-03 11:18:30 +0100

    Refactor FizzBuzz logic into separate Rules class
    Moved the FizzBuzz computation logic from FizzBuzz.cs to a new Rules.cs class to enhance modularity and code organization. This change allows for clearer separation of responsibilities and makes the FizzBuzz rules reusable across different parts of the application.

--- exercise/C#/day02/Games/FizzBuzz.cs
+++ exercise/C#/day02/Games/FizzBuzz.cs
@@ -14,30 +14,7 @@ public static class FizzBuzz
             : ConvertSafely(input);
     }
 
-    private static string ConvertSafely(int input)
-    {
-        var isFizz = Is(3, input);
-        var isBuzz = Is(5, input);
-        var isWhizz = Is(7, input);
-        var isBang = Is(11, input);
-
-        return (isFizz, isBuzz, isWhizz, isBang) switch
-        {
-            (true, true, false, false) => "FizzBuzz",
-            (true, false, true, false) => "FizzWhizz",
-            (true, false, false, true) => "FizzBang",
-            (true, false, false, false) => "Fizz",
-            (false, true, false, false) => "Buzz",
-            (false, false, true, false) => "Whizz",
-            (false, false, false, true) => "Bang",
-            _ => input.ToString()
-        };
-    }
-    
-    private static bool Is(int divisor, int input)
-    {
-        return input % divisor == 0;
-    }
+    private static string ConvertSafely(int input) => Rules.FizzBuzz(input);
 
     private static bool IsOutOfRange(int input)
     {
---
+++ exercise/C#/day02/Games/Rules.cs
@@ -0,0 +1,21 @@
+﻿namespace Games;
+
+public static class Rules
+{
+    public static string FizzBuzz(int input)
+    {
+        return (isFizz: Is(3, input), isBuzz: Is(5, input), isWhizz: Is(7, input), isBang: Is(11, input)) switch
+        {
+            (true, true, false, false) => "FizzBuzz",
+            (true, false, true, false) => "FizzWhizz",
+            (true, false, false, true) => "FizzBang",
+            (true, false, false, false) => "Fizz",
+            (false, true, false, false) => "Buzz",
+            (false, false, true, false) => "Whizz",
+            (false, false, false, true) => "Bang",
+            _ => input.ToString()
+        };
+
+        static bool Is(int divisor, int input) => input % divisor == 0;
+    }
+}
